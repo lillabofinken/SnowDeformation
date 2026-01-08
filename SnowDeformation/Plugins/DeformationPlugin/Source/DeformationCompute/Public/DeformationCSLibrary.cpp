d51123cd612f6ff41c0517cc6efcdcdd4ba6153b
Marcus Philip Valfred Bäcklund <philip.backlund@learnet.se>
2026-01-08 11:26:33 +0100

    Deformation Library added

--- SnowDeformation/Plugins/DeformationPlugin/Source/DeformationCompute/Public/DeformationCSLibrary.cpp
+++ SnowDeformation/Plugins/DeformationPlugin/Source/DeformationCompute/Public/DeformationCSLibrary.cpp
@@ -1,1 +1,10 @@
 #include "DeformationCSLibrary.h"
+#include "DeformationCS/DeformationCS.h"
+void UDeformationCSLibrary::ExecuteRTComputeShader(UTextureRenderTarget2D* RT)
+{
+	//Create a dispatch parameters struct and fill it the input array with our args
+	FDeformationCSDispatchParams Params(RT->SizeX, RT->SizeY, 1);
+	Params.RenderTarget = RT->GameThread_GetRenderTargetResource();
+	
+	FDeformationCSInterface::Dispatch(Params);
+}
--- SnowDeformation/Plugins/DeformationPlugin/Source/DeformationCompute/Public/DeformationCSLibrary.h
+++ SnowDeformation/Plugins/DeformationPlugin/Source/DeformationCompute/Public/DeformationCSLibrary.h
@@ -1,7 +1,19 @@
 #pragma once
 
-class DeformationCSLibrary
+#include "CoreMinimal.h"
+#include "GenericPlatform/GenericPlatformMisc.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "Engine/TextureRenderTarget2D.h"
+#include "Materials/MaterialRenderProxy.h"
+
+#include "DeformationCSLibrary.generated.h"
+//This is a static blueprint library that can be used to invoke our compute shader from blueprints.
+UCLASS()
+class DEFORMATIONCOMPUTE_API UDeformationCSLibrary : public UObject
 {
+	GENERATED_BODY()
+
 public:
-	
+	UFUNCTION(BlueprintCallable)
+	static void ExecuteRTComputeShader(UTextureRenderTarget2D* RT);
 };
